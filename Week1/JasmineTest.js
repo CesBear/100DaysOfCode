2913a0d28a5b103a1401d69e9b2dbcb1ca4b93c8
Cesar Ramirez <Cesar_Ramirez@epam.com>
2017-09-11 11:46:43 -0500

    Adding read me

--- Week1/JasmineTest.js
+++ Week1/JasmineTest.js
@@ -2,17 +2,15 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
-});
-
-
-describe ("Second test", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
-  });
-});
 
-describe ("This is a nee test",. function() {
   it("Contains secon client", function() {
     expect(value2).toBe(false);
   });
+
+  it ("Should have step and ", function() {
+      expect(false).toBe(false);
+  });
+
 });
---
+++ Week1/README.md
@@ -0,0 +1,7 @@
+### Week 1
+
+## About
+In this week I'll be working with Jasmine  framework
+
+## Specs
+To write a set of test with JAsmine frameworks
