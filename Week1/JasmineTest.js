cf44b024338f67d84e8ac2226ea8c18fc4b58766
Cesar Ramirez <Cesar_Ramirez@epam.com>
2017-09-10 23:15:13 -0500

    v3

--- Week1/JasmineTest.js
+++ Week1/JasmineTest.js
@@ -5,9 +5,14 @@ describe("A suite", function() {
 });
 
 
-
 describe ("Second test", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
+  });
+});
+
+describe ("This is a nee test",. function() {
+  it("Contains secon client", function() {
+    expect(value2).toBe(false);
   });
 });
