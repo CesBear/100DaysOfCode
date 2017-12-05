96f9ce118b9af8e4a7f58d3ef6e6ecf51621ed55
Cesar Ramirez <Cesar_Ramirez@epam.com>
2017-12-05 12:56:38 -0600

    updated js file

--- Week2/scripts.js
+++ Week2/scripts.js
@@ -1,6 +1,13 @@
 var name;
-
 name = 'Cesar';
-
-
 console.log(name);
+
+
+var ageAsString = promt("whats is your age?", "");
+var age = Number(ageAsString);
+
+if (age < 40) {
+  alert("Oh you're so young ...");
+}
+
+alert ("Thank you");
